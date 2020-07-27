f0e68dac876329a25d287e95292da4a8d273695d
lirenhao <renhao.li@bjyada.com>
2020-07-27 12:39:40 +0800

    svc init

---
+++ svc/src/test/java/com/yada/ssp/manager/svc/SvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yada.ssp.manager.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
