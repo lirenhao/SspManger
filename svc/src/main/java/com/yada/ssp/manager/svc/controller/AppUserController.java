88521eaa3a87583d5df2e79db14a1b0112185189
lirenhao <renhao.li@bjyada.com>
2020-07-28 14:17:57 +0800

    API修改

--- svc/src/main/java/com/yada/ssp/manager/svc/controller/AppUserController.java
+++ svc/src/main/java/com/yada/ssp/manager/svc/controller/AppUserController.java
@@ -1,20 +1,17 @@
 package com.yada.ssp.manager.svc.controller;
 
 import com.yada.ssp.manager.svc.auth.model.Auth;
-import com.yada.ssp.manager.svc.model.*;
+import com.yada.ssp.manager.svc.model.AppUser;
+import com.yada.ssp.manager.svc.model.AppUserCheck;
+import com.yada.ssp.manager.svc.model.AppUserPK;
 import com.yada.ssp.manager.svc.query.AppUserCheckQuery;
-import com.yada.ssp.manager.svc.query.TerminalQuery;
-import com.yada.ssp.manager.svc.service.*;
+import com.yada.ssp.manager.svc.service.AppUserCheckService;
+import com.yada.ssp.manager.svc.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 /**
  * Merchant APP用户API
@@ -24,173 +21,58 @@ import java.util.List;
 public class AppUserController {
 
     private final AppUserCheckService appUserCheckService;
-    private final AppRoleService appRoleService;
-    private final MerchantService merchantService;
-    private final CcyTypeService ccyTypeService;
-    private final TerminalService terminalService;
     private final AppUserService appUserService;
 
     @Autowired
-    public AppUserController(AppUserCheckService appUserCheckService, AppRoleService appRoleService,
-                             MerchantService merchantService, CcyTypeService ccyTypeService,
-                             TerminalService terminalService, AppUserService appUserService) {
+    public AppUserController(AppUserCheckService appUserCheckService, AppUserService appUserService) {
         this.appUserCheckService = appUserCheckService;
-        this.appRoleService = appRoleService;
-        this.merchantService = merchantService;
-        this.ccyTypeService = ccyTypeService;
-        this.terminalService = terminalService;
         this.appUserService = appUserService;
     }
 
-    @RequestMapping("/list")
-    public String list(Model model, @RequestAttribute("auth") Auth auth,
-                       @ModelAttribute AppUserCheckQuery query, @PageableDefault Pageable pageable) {
+    @GetMapping
+    public Page<AppUserCheck> list(@RequestAttribute("auth") Auth auth,
+                                   @ModelAttribute AppUserCheckQuery query, @PageableDefault Pageable pageable) {
         query.setOrgId(auth.getOrgId());
-        Page<AppUserCheck> page = appUserCheckService.findAll(query, pageable);
-        model.addAttribute("query", query);
-        model.addAttribute("page", page);
-        return "ssp_pages/AppUser/list";
+        return appUserCheckService.findAll(query, pageable);
     }
 
-    @RequestMapping("/listForCheck")
-    public String listForCheck(Model model, @RequestAttribute("auth") Auth auth,
-                               @ModelAttribute AppUserCheckQuery query, @PageableDefault Pageable pageable) {
-        query.setOrgId(auth.getOrgId());
-        Page page = appUserCheckService.findAll(query, pageable);
-        model.addAttribute("query", query);
-        model.addAttribute("page", page);
-        return "ssp_pages/AppUser/listForCheck";
-    }
-
-    @RequestMapping("/create")
-    public String create(Model model, @RequestAttribute("auth") Auth auth) {
-        List<CcyType> ccyTypeList = ccyTypeService.findAll();
-        List<Merchant> merchantList = merchantService.findByOrgId(auth.getOrgId());
-        List<AppRole> roleList = appRoleService.findAll();
-        model.addAttribute("merchantList", merchantList);
-        model.addAttribute("roleList", roleList);
-        model.addAttribute("ccyTypeList", ccyTypeList);
-        return "ssp_pages/AppUser/create";
-    }
-
-    @RequestMapping("/save")
-    public String save(@ModelAttribute("model") AppUserCheck appUserCheck) {
+    @PostMapping
+    public void save(@ModelAttribute AppUserCheck appUserCheck) {
         appUserCheckService.saveAndUpdate(appUserCheck);
-        return "redirect:list";
     }
 
-    @RequestMapping("/updateUser")
-    public String updateUser(String merNo, String loginName, String userName, String roles, String termNo, String ccyType) {
+    @PutMapping
+    public void update(String merNo, String loginName, String userName, String roles, String termNo, String ccyType) {
         appUserCheckService.updateUser(new AppUserPK(merNo, loginName), userName, roles, termNo, ccyType);
-        return "redirect:list";
     }
 
-    @RequestMapping("/show")
-    public String show(Model model, String merNo, String loginName) {
-        AppUserCheck appUserCheck = appUserCheckService.findOne(new AppUserPK(merNo, loginName));
-        if (appUserCheck.getRoles() != null && !appUserCheck.getRoles().equals("")) {
-            String[] userRoleList = appUserCheck.getRoles().split(",");
-            model.addAttribute("userRoleList", userRoleList);
-        }
-        List<AppRole> roleList = appRoleService.findAll();
-        model.addAttribute("roleList", roleList);
-        model.addAttribute("model", appUserCheck);
-        return "ssp_pages/AppUser/show";
+    @GetMapping("/{merNo}/{loginName}")
+    public AppUserCheck show(@PathVariable String merNo, @PathVariable String loginName) {
+        return appUserCheckService.findOne(new AppUserPK(merNo, loginName));
     }
 
-    @RequestMapping("/showForCheck")
-    public String showForCheck(Model model, String merNo, String loginName) {
-        AppUserCheck appUserCheck = appUserCheckService.findOne(new AppUserPK(merNo, loginName));
-        if (appUserCheck.getRoles() != null && !appUserCheck.getRoles().equals("")) {
-            String[] userRoleList = appUserCheck.getRoles().split(",");
-            model.addAttribute("userRoleCheckList", userRoleList);
-        }
-        AppUser appUser = appUserService.findOne(new AppUserPK(merNo, loginName));
-        if (appUser != null && appUser.getRoles() != null && !appUser.getRoles().equals("")) {
-            String[] userRoleList = appUser.getRoles().split(",");
-            model.addAttribute("userRoleList", userRoleList);
-        }
-        List<AppRole> roleList = appRoleService.findAll();
-        model.addAttribute("appUser", appUser);
-        model.addAttribute("roleList", roleList);
-        model.addAttribute("model", appUserCheck);
-        return "ssp_pages/AppUser/showForCheck";
+    @GetMapping("/{merNo}/{loginName}/check")
+    public AppUser showCheck(@PathVariable String merNo, @PathVariable String loginName) {
+        return appUserService.findOne(new AppUserPK(merNo, loginName));
     }
 
-    @RequestMapping("/check")
-    public String check(Model model, String merNo, String loginName) {
-        AppUserCheck appUserCheck = appUserCheckService.findOne(new AppUserPK(merNo, loginName));
-        if (appUserCheck.getRoles() != null && !appUserCheck.getRoles().equals("")) {
-            String[] userRoleList = appUserCheck.getRoles().split(",");
-            model.addAttribute("userRoleCheckList", userRoleList);
-        }
-        AppUser appUser = appUserService.findOne(new AppUserPK(merNo, loginName));
-        if (appUser != null && appUser.getRoles() != null && !appUser.getRoles().equals("")) {
-            String[] userRoleList = appUser.getRoles().split(",");
-            model.addAttribute("userRoleList", userRoleList);
-        }
-        List<AppRole> roleList = appRoleService.findAll();
-        model.addAttribute("appUser", appUser);
-        model.addAttribute("roleList", roleList);
-        model.addAttribute("model", appUserCheck);
-        return "ssp_pages/AppUser/check";
-    }
-
-    @RequestMapping("/saveCheck")
-    public String saveCheck(String state, String checkReason, String merNo, String loginName) {
+    @PutMapping("/{merNo}/{loginName}/check")
+    public void saveCheck(@PathVariable String merNo, @PathVariable String loginName, String state, String checkReason) {
         appUserCheckService.saveCheck(state, checkReason, merNo, loginName);
-        return "redirect:listForCheck";
     }
 
-    @RequestMapping("/edit")
-    public String edit(Model model, String merNo, String loginName) {
-        AppUserCheck appUserCheck = appUserCheckService.findOne(new AppUserPK(merNo, loginName));
-        if (appUserCheck.getRoles() != null && !appUserCheck.getRoles().equals("")) {
-            String[] userRoleList = appUserCheck.getRoles().split(",");
-            model.addAttribute("userRoleList", userRoleList);
-        }
-        List<CcyType> ccyTypeList = ccyTypeService.findAll();
-        List<AppRole> roleList = appRoleService.findAll();
-        Merchant merchant = merchantService.findOne(merNo);
-        TerminalQuery terminalQuery = new TerminalQuery();
-        terminalQuery.setMerchantId(appUserCheck.getMerNo());
-        List<Terminal> terminalList = terminalService.findAll(terminalQuery);
-        model.addAttribute("merchant", merchant);
-        model.addAttribute("roleList", roleList);
-        model.addAttribute("model", appUserCheck);
-        model.addAttribute("ccyTypeList", ccyTypeList);
-        model.addAttribute("terminalList", terminalList);
-        return "ssp_pages/AppUser/edit1";
-    }
-
-    @RequestMapping("/resetPassword")
-    public String resetPassword(String merNo, String loginName, RedirectAttributes redirectAttrs) {
+    @PutMapping("/{merNo}/{loginName}/resetPwd")
+    public void resetPassword(@PathVariable String merNo, @PathVariable String loginName) {
         appUserService.resetPassword(new AppUserPK(merNo, loginName));
-        redirectAttrs.addFlashAttribute("message", String.format("用户[%s@%s]密码已重置!", merNo, loginName));
-        return "redirect:list";
     }
 
-    @RequestMapping("/delete")
-    public String delete(String merNo, String loginName) {
+    @DeleteMapping("/{merNo}/{loginName}")
+    public void delete(@PathVariable String merNo, @PathVariable String loginName) {
         appUserCheckService.delete(new AppUserPK(merNo, loginName));
-        return "redirect:list";
     }
 
-    @ResponseBody
-    @RequestMapping("/AJAX_findUserByMerNoAndLoginName")
-    public String AJAX_findUserByMerNoAndLoginName(String merNo, String loginName) {
-        String mess = "*";
-        if (appUserCheckService.exists(new AppUserPK(merNo, loginName))) {
-            mess = "该商户下此登录名已存在！";
-        }
-        return mess;
-    }
-
-    @ResponseBody
-    @RequestMapping("/AJAX_findTerminalByMerchantId")
-    public List<Terminal> AJAX_findTerminalByMerchantId(String merchantId) {
-        TerminalQuery terminalQuery = new TerminalQuery();
-        terminalQuery.setMerchantId(merchantId);
-        return terminalService.findAll(terminalQuery);
+    @GetMapping("/{merNo}/{loginName}/exists")
+    public boolean exists(@PathVariable String merNo, @PathVariable String loginName) {
+        return appUserCheckService.exists(new AppUserPK(merNo, loginName));
     }
 }
--- svc/src/main/java/com/yada/ssp/manager/svc/controller/CcpayController.java
+++ svc/src/main/java/com/yada/ssp/manager/svc/controller/CcpayController.java
@@ -1,192 +1,106 @@
 package com.yada.ssp.manager.svc.controller;
 
 import com.yada.ssp.manager.svc.auth.model.Auth;
+import com.yada.ssp.manager.svc.model.Ccpay;
 import com.yada.ssp.manager.svc.model.CcpayCheck;
-import com.yada.ssp.manager.svc.model.Merchant;
 import com.yada.ssp.manager.svc.net.SspResult;
 import com.yada.ssp.manager.svc.query.CcpayCheckQuery;
-import com.yada.ssp.manager.svc.query.MerchantQuery;
 import com.yada.ssp.manager.svc.service.CcpayCheckService;
 import com.yada.ssp.manager.svc.service.CcpayService;
-import com.yada.ssp.manager.svc.service.MerchantService;
 import com.yada.ssp.manager.svc.service.SspService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 /**
- * Ccpay参数配置API
+ * CCPAY参数配置API
  */
-
 @RestController
 @RequestMapping("/ccpay")
 public class CcpayController {
 
     private final CcpayCheckService ccpayCheckService;
-    private final MerchantService merchantService;
-    private final SspService sspService;
     private final CcpayService ccpayService;
+    private final SspService sspService;
+
     @Autowired
-    public CcpayController(CcpayCheckService ccpayCheckService, MerchantService merchantService, SspService sspService, CcpayService ccpayService) {
+    public CcpayController(CcpayCheckService ccpayCheckService, CcpayService ccpayService, SspService sspService) {
         this.ccpayCheckService = ccpayCheckService;
-        this.merchantService = merchantService;
-        this.sspService = sspService;
         this.ccpayService = ccpayService;
+        this.sspService = sspService;
     }
 
-    @RequestMapping("/list")
-    public String list(Model model, @RequestAttribute("auth") Auth auth,
-                       @ModelAttribute CcpayCheckQuery query, @PageableDefault Pageable pageable) {
+    @GetMapping
+    public Page<CcpayCheck> list(@RequestAttribute("auth") Auth auth,
+                                 @ModelAttribute CcpayCheckQuery query, @PageableDefault Pageable pageable) {
         query.setOrgId(auth.getOrgId());
-        Page page = ccpayCheckService.findAll(query, pageable);
-        model.addAttribute("query", query);
-        model.addAttribute("page", page);
-        return "ssp_pages/Ccpay/list";
+        return ccpayCheckService.findAll(query, pageable);
     }
 
-    @RequestMapping("/listForCheck")
-    public String listForCheck(Model model, @RequestAttribute("auth") Auth auth,
-                               @ModelAttribute CcpayCheckQuery query, @PageableDefault Pageable pageable) {
-        query.setOrgId(auth.getOrgId());
-        Page page = ccpayCheckService.findAll(query, pageable);
-        model.addAttribute("query", query);
-        model.addAttribute("page", page);
-        return "ssp_pages/Ccpay/listForCheck";
-    }
-
-    @RequestMapping("/create")
-    public String create(Model model, @RequestAttribute("auth") Auth auth) {
-        List<Merchant> merchantList = merchantService.findByOrgId(auth.getOrgId());
-        model.addAttribute("merchantList", merchantList);
-        return "ssp_pages/Ccpay/create";
-    }
-
-    @RequestMapping("/save")
-    public String save(@ModelAttribute("model") CcpayCheck ccpayCheck) {
+    @PostMapping
+    public void save(@ModelAttribute CcpayCheck ccpayCheck) {
         ccpayCheckService.save(ccpayCheck);
-        return "redirect:list";
     }
 
-    @RequestMapping("/saveCheck")
-    public String saveCheck(String merchantId, String checkReason, String state) {
-        ccpayCheckService.saveCheck(merchantId,checkReason,state);
-        return "redirect:listForCheck";
-    }
-
-    @RequestMapping("/update")
-    public String update(@ModelAttribute("model") CcpayCheck ccpayCheck) {
+    @PutMapping
+    public void update(@ModelAttribute CcpayCheck ccpayCheck) {
         ccpayCheckService.update(ccpayCheck);
-        return "redirect:list";
     }
 
-    /**
-     * 修改静态码,不用审核
-     *
-     * @param merchantId 商户号
-     * @return 重定向页面
-     */
-    @RequestMapping("/updateStaticQrc")
-    public String updateStaticQrc(String merchantId, RedirectAttributes redirectAttributes) {
-        SspResult sspResult = sspService.getStaticCode(merchantId);
-        String message;
-        if (sspResult.getRespCode().equals("success")) {
-            ccpayService.updateStaticQrc(merchantId, sspResult.getQrCode());
-            message = "商户号为" + merchantId + "商户获取静态码成功";
-        } else {
-            message = "商户号为" + merchantId + "商户获取静态码失败,失败原因" + sspResult.getRespMsg();
-        }
-        redirectAttributes.addFlashAttribute("message", message);
-        return "redirect:list";
+    @DeleteMapping("/{merchantId}")
+    public void delete(@PathVariable String merchantId) {
+        ccpayCheckService.delete(merchantId);
     }
 
-    /**
-     * 修改通知状态,不用审核
-     *
-     * @param merchantId 商户号
-     * @return 重定向页面
-     */
-    @RequestMapping("/updateNotifyFlag")
-    public String updateNotifyFlag(String merchantId, RedirectAttributes redirectAttributes) {
-        String message;
-        SspResult sspResult = sspService.setCallbackPath(merchantId);
-        if (sspResult.getRespCode().equals("success")) {
-            ccpayService.updateNotifyFlag(merchantId, "1");
-            message = "商户号为" + merchantId + "商户设置通知地址成功";
-        } else {
-            message = "商户号为" + merchantId + "商户设置通知地址失败,失败原因" + sspResult.getRespMsg();
-        }
-        redirectAttributes.addFlashAttribute("message", message);
-        return "redirect:list";
-    }
-
-    @RequestMapping("/show")
-    public String show(Model model, String merchantId) {
-        String staticQrc = ccpayService.findOne(merchantId).getStaticQrc();
-        model.addAttribute("model", ccpayCheckService.findOne(merchantId));
-        model.addAttribute("staticQrc",staticQrc);
-        return "ssp_pages/Ccpay/show";
-    }
-
-    @RequestMapping("/showForCheck")
-    public String showForCheck(Model model, String merchantId) {
-        model.addAttribute("model", ccpayCheckService.findOne(merchantId));
-        model.addAttribute("ccpay", ccpayService.findOne(merchantId));
-        return "ssp_pages/Ccpay/showForCheck";
-    }
-
-    @RequestMapping("/check")
-    public String check(Model model, String merchantId) {
-        model.addAttribute("model", ccpayCheckService.findOne(merchantId));
-        model.addAttribute("ccpay", ccpayService.findOne(merchantId));
-        return "ssp_pages/Ccpay/check";
-    }
-
-    @RequestMapping("/edit")
-    public String edit(Model model, @RequestAttribute("auth") Auth auth, String merchantId) {
-        MerchantQuery merchantQuery = new MerchantQuery();
-        merchantQuery.setOrgId(auth.getOrgId());
-        List<Merchant> merchantList = merchantService.findByOrgId(auth.getOrgId());
+    @GetMapping("/{merchantId}")
+    public CcpayCheck get(@PathVariable String merchantId) {
         CcpayCheck ccpayCheck = ccpayCheckService.findOne(merchantId);
         if (ccpayCheck.getFee() != null) {
             BigDecimal fee = ccpayCheck.getFee();
             fee = fee.multiply(new BigDecimal(1000)).setScale(0);
             ccpayCheck.setFee(fee);
         }
-        model.addAttribute("merchantList", merchantList);
-        model.addAttribute("model", ccpayCheck);
-        return "ssp_pages/Ccpay/edit1";
+        return ccpayCheck;
     }
 
-    @RequestMapping("/delete")
-    public String delete(String merchantId){
-        ccpayCheckService.delete(merchantId);
-        return "redirect:list";
+    @GetMapping("/{merchantId}/check")
+    public Ccpay getCheck(@PathVariable String merchantId) {
+        return ccpayService.findOne(merchantId);
+    }
+
+    @PutMapping("/{merchantId}/check")
+    public void saveCheck(@PathVariable String merchantId, String checkReason, String state) {
+        ccpayCheckService.saveCheck(merchantId, checkReason, state);
     }
 
     /**
-     * 查询该商户号是否已有记录
+     * 修改静态码,不用审核
      *
      * @param merchantId 商户号
-     * @return 返回信息
+     * @return 结果
      */
-    @ResponseBody
-    @RequestMapping("/AJAX_findByMerchantId")
-    public String AJAX_findByMerchantId(String merchantId) {
-        String mess = "*";
-        if (ccpayCheckService.exists(merchantId)) {
-            mess = "该商户已存在！";
-        }
-        return mess;
+    @PutMapping("/{merchantId}/staticQrc")
+    public SspResult updateStaticQrc(@PathVariable String merchantId) {
+        return sspService.getStaticCode(merchantId);
+    }
+
+    /**
+     * 修改通知状态,不用审核
+     *
+     * @param merchantId 商户号
+     * @return 结果
+     */
+    @PutMapping("/{merchantId}/notifyFlag")
+    public SspResult updateNotifyFlag(@PathVariable String merchantId) {
+        return sspService.setCallbackPath(merchantId);
+    }
+
+    @GetMapping("/{merchantId}/exists")
+    public boolean exists(@PathVariable String merchantId) {
+        return ccpayCheckService.exists(merchantId);
     }
 }
