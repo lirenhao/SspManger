bd33b54efacdf0ee8af8d55787c2af04e5e9e747
lirenhao <renhao.li@bjyada.com>
2020-07-29 14:58:04 +0800

    修改目录名称

--- app/src/pages/systemParam/MccCode/locales/en-US.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  'mcc.mcc': 'mcc',
-  'mcc.mccNameNecessary': 'mcc 是必须的',
-  'mcc.remark': 'remark',
-  'mcc.createCompoent': '新建mcc',
-  'mcc.updateCompoent': '修改mcc',
-  'mcc.delete': '删除',
-};
--- app/src/pages/systemParam/MccCode/locales/zh-CN.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  'mcc.mcc': 'mcc',
-  'mcc.mccNecessary': 'mcc 是必须的',
-  'mcc.remark': 'remark',
-  'mcc.createCompoent': '新建mcc',
-  'mcc.updateCompoent': '修改mcc',
-  'mcc.delete': '删除',
-  'mcc.create': '增加',
-};
