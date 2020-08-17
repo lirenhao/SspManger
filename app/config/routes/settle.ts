6082f016e3c87a586ec3b0b356b69464491f1aa2
zhmouohz <zhmouohz@163.com>
2020-08-17 13:53:50 +0800

    漏提交的文件

---
+++ app/config/routes/settle.ts
@@ -0,0 +1,12 @@
+import { IRoute } from 'umi';
+
+const routes: IRoute[] = [
+  {
+    path: '/settle/cup',
+    name: 'cup',
+    component: './settle/cup',
+    locale: 'settle.cup',
+  },
+];
+
+export default routes;
