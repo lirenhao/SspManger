3953e64c1a3fe0eeccd19e1fb82113cd1a2e2816
lirenhao <renhao.li@bjyada.com>
2020-09-02 15:43:43 +0800

    admin更新

--- admin/src/pages/changePwd/service.ts
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request';
-import { ModifyData } from './data';
-
-export async function changePwd(params: ModifyData) {
-  return request('/admin/apis/change_pwd', {
-    method: 'POST',
-    data: params,
-  });
-}
